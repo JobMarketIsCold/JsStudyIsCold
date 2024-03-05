7d4c079af295f8c95fe920bfac39359144ed98aa
황현민 <hwanghyunmin0650@gmail.com>
2024-03-05 11:27:03 +0900

    JS Calculator
    심플 계산기

--- momentum/app.js
+++ momentum/app.js
@@ -1,8 +1,23 @@
-const player = {
-	name: "200won",
-	HelloFunc: function (Name) {
-		console.log("Hello! " + Name);
+const calculator = {
+	plus: function (a, b) {
+		console.log(a + b);
+	},
+	minus: function (a, b) {
+		console.log(a - b);
+	},
+	times: function (a, b) {
+		console.log(a * b);
+	},
+	divide: function (a, b) {
+		console.log(a / b);
+	},
+	power: function (a) {
+		console.log(a * a);
 	},
 };
 
-player.HelloFunc(player.name);
+calculator.plus(5, 8);
+calculator.minus(5, 8);
+calculator.times(5, 8);
+calculator.divide(5, 8);
+calculator.power(5, 8);
