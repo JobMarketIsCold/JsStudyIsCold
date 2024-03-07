5cf708c59659a2f5abc28e887c8b707728366200
황현민 <hwanghyunmin0650@gmail.com>
2024-03-07 17:37:49 +0900

    all done!

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -4,8 +4,15 @@ function onGeoOk(position) {
 	const lat = position.coords.latitude;
 	const lon = position.coords.longitude;
 	console.log(`너는 ${lat},${lon}에 살고 있지?`);
-	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-	console.log(url);
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+			const weather = document.querySelector("#weather span:first-child");
+			const city = document.querySelector("#weather span:last-child");
+			weather.innerText = `${data.weather[0].main} / ${data.main.temp}도`;
+			city.innerText = data.name;
+		});
 }
 
 function onGeoError() {
